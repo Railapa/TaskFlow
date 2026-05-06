8a374da6d080965cbe606ff525e4e333cf6efe0c
Railapa <rai100304@gmail.com>
2026-05-06 20:43:23 -0300

    TaskFlow

--- js/script.js
+++ js/script.js
@@ -29,16 +29,64 @@ btnCancelar.addEventListener('click', () => {
 })
 
 
-const aside = document.querySelector('#aside')
 const overlay = document.querySelector('#overlay')
+const aside = document.querySelector('#aside')
 const menuHamburguer = document.querySelector('.menuHamburguer')
 
 menuHamburguer.addEventListener('click', () => {
     aside.classList.toggle('show')
-    overlay.classList.toggle('active')
+    overlay.classList.toggle('overlay-active')
 })
 
 overlay.addEventListener('click', () => {
+    overlay.classList.remove('overlay-active')
     aside.classList.remove('show')
-    overlay.classList.remove('active')
+})
+
+const in_titulo = document.querySelector('#in_titulo')
+const in_descricao = document.querySelector('#in_descricao')
+const in_categoria = document.querySelector('#in_categoria')
+const in_prioridade = document.querySelector('#in_prioridade')
+const in_data = document.querySelector('#in_data')
+const btnAdd = document.querySelector('.btnAdd')
+const cards = document.querySelector('.cards')
+const num_tarefas = document.querySelector('.num_tarefas')
+
+const text_categoria = document.querySelector('.text_categoria')
+const text_prioridade = document.querySelector('.text_prioridade')
+const card = document.querySelector('.card')
+
+btnAdd.addEventListener('click', () => {
+    const div = document.createElement('div')
+    
+    div.innerHTML += `<div class="card">
+                <div class="cardHeader">
+                    <div class="cardRadio">
+                        <button>
+                            <i class="fa-regular fa-circle"></i>
+                        </button>
+                    </div>
+
+                    <div class="cardContent">
+                        <h3>${in_titulo.value}</h3>
+                        <p>${in_descricao.value}</p>
+                    </div>
+                </div>
+                <div class="cardLevel">
+                    <span>${in_categoria.value}</span>
+                    <span>${in_prioridade.value}</span>
+                </div>
+
+                <div class="modalData">
+                    <span>${in_data.value}</span>
+                </div>
+            </div>`
+
+    in_titulo.value = ''
+    in_descricao.value = ''
+    in_categoria.value = ''
+    in_prioridade.value = ''
+    in_data.value = ''
+    modal.close()
+    cards.appendChild(div)
 })
